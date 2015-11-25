be4ac2db9000a792432dacb1c4532aae490110a9
Yanan <yanan@pocketgems.com>
2015-11-24 16:10:26 -0800

    Create the new project

---
+++ UploadMaster/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  UploadMaster
+//
+//  Created by Yanan Li on 11/24/15.
+//  Copyright © 2015 Yanan Li. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ UploadMaster/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  UploadMaster
+//
+//  Created by Yanan Li on 11/24/15.
+//  Copyright © 2015 Yanan Li. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
